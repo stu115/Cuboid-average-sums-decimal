07223a55ac4d1cd4384f136f888307beb021159c
Stefan Ahmed <32064735+stu115@users.noreply.github.com>
2022-02-27 23:49:17 +0000

    Add files via upload

---
+++ Week 3/Q1.java
@@ -0,0 +1,19 @@
+package week3;
+
+public class Q1 {
+	
+	public static void main(String[]args)
+	
+	//Two integers are being declared and the use of +,-,*,/ and % being demonstrated
+	{
+		int abi = 8;
+		int jenny = 5; 
+		
+		System.out.println("Adding 8 to 5 is "+(abi+jenny));
+		System.out.println("Subtracting 5 from 8 is "+(abi-jenny));
+		System.out.println("Multiplying 8 by 5 is "+(abi*jenny));
+		System.out.println("The division of 8 by 5 is "+(abi/jenny));
+		System.out.println("The remainder of 8%5 is " +(abi%jenny));
+	}
+
+}
---
+++ Week 3/Q3.java
@@ -0,0 +1,14 @@
+package week3;
+
+public class Q3 {
+	
+	public static void main(String[]args)
+	
+	//This shows the sum of the first 10 positive prime numbers
+	
+	{
+		System.out.println(2+3+5+7+11+13+17+19+23+29);
+		
+	}
+
+}
